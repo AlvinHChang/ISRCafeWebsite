06d456f8da639802371a487411da612f64b09007
Alvin Chang <ahchang6@illinois.edu>
2019-05-03 13:36:44 -0500

    Initial

---
+++ precache-manifest.40084c67482bf0477e8ccecb8a540588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "38fab796364bdeb896d9",
+    "url": "/static/css/main.ed4a1799.chunk.css"
+  },
+  {
+    "revision": "38fab796364bdeb896d9",
+    "url": "/static/js/main.38fab796.chunk.js"
+  },
+  {
+    "revision": "d0c10f04c564c81f4c3d",
+    "url": "/static/js/1.d0c10f04.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb5d1da33569f3cc94894e715e22a8d8",
+    "url": "/static/media/PetrolIcon.bb5d1da3.png"
+  },
+  {
+    "revision": "84f9f08b7381dd54ef93adb418ac3705",
+    "url": "/static/media/afrocalebCrop.84f9f08b.png"
+  },
+  {
+    "revision": "f6429affefa90a85bb8b599540907c59",
+    "url": "/index.html"
+  }
+];
